7f6146774ad14d750386a3326eaaebe0cc7490e3
lc <156845259@qq.com>
2019-09-03 22:38:44 +0800

    添加数组、资源及字符串扩展类

---
+++ ResourceHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace EMap
+{
+    public static class ResourceHelper
+    {
+        /// <summary>
+        /// 根据项目中的资源名称获取其实际存放的路径
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="assemblyName"></param>
+        /// <returns></returns>
+        public static string GetFileName(string name, string assemblyName)
+        {
+            System.Reflection.Assembly asm = AppDomain.CurrentDomain.GetAssemblies().FirstOrDefault(x => x.GetName().Name == assemblyName); 
+            string fileName = GetFileName(name, asm);
+            return fileName;
+        }
+        public static string GetFileName(string name, Assembly asm)
+        {
+            string fileName = null;
+            if (string.IsNullOrEmpty(name) || asm == null)
+            {
+                return fileName;
+            }
+            string directory = Path.GetDirectoryName(asm.CodeBase).Replace("file:\\", "");
+            fileName = Path.Combine(directory, name);
+            return fileName;
+        }
+    }
+}
---
+++ StringExtension.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace EMap
+{
+    public static class StringExtension
+    {
+        public static bool ToPosition(this string str, out double x, out double y)
+        {
+            bool ret = false;
+            x = 0;
+            y = 0;
+            if (string.IsNullOrWhiteSpace(str) || !str.Contains(' '))
+            {
+                return ret;
+            }
+            string[] array = str.Split(' ');
+            if (array.Length != 2)
+            {
+                return ret;
+            }
+            ret = double.TryParse(array[0], out x);
+            if (ret)
+            {
+                ret = double.TryParse(array[1], out y);
+            }
+            return ret;
+        }
+    }
+}
