8504c2438ca08e7520e284867e647181b1235462
鲁闯 <156845259@qq.com>
2022-11-12 21:19:42 +0800

    修改下载地图

---
+++ EM.GIS.Tools/Models/WindowExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using System.Windows;
+
+namespace EM.GIS.Tools
+{
+    public static class WindowExtensions
+    {
+        /// <summary>
+        /// 获取选择的文件夹
+        /// </summary>
+        /// <param name="title">标题</param>
+        /// <param name="window">窗体</param>
+        /// <returns>文件夹</returns>
+        public static string GetSelectedFolder(string title = "请选择目录", Window window = null)
+        {
+            string folder = string.Empty;
+            CommonOpenFileDialog dg = new CommonOpenFileDialog()
+            {
+                IsFolderPicker = true,
+                Title =title
+            };
+            if (dg.ShowDialog(window)== CommonFileDialogResult.Ok)
+            {
+                folder=dg.FileName;
+            }
+            return folder;
+        }
+    }
+}
---
+++ EM.GIS.Tools/ViewModels/ModifyLandsatMtlViewModel.cs
@@ -0,0 +1,107 @@
+﻿using EM.Bases;
+using EM.WpfBases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace EM.GIS.Tools
+{
+    public class ModifyLandsatMtlViewModel : ViewModel<ModifyLandsatMtlControl>
+    {
+        private string _srcDirectory;
+        /// <summary>
+        /// 数据目录
+        /// </summary>
+        public string SrcDirectory
+        {
+            get { return _srcDirectory; }
+            set { SetProperty(ref _srcDirectory, value); }
+        }
+        public DelegateCommand BrowseCmd { get; }
+        public DelegateCommand ExcuteCmd { get; }
+        public ModifyLandsatMtlViewModel(ModifyLandsatMtlControl t) : base(t)
+        {
+            ExcuteCmd=new DelegateCommand(Excute, CanExcute);
+            BrowseCmd=new DelegateCommand(Browse);
+            PropertyChanged+=ModifyLandsatMtlViewModel_PropertyChanged;
+        }
+
+        private void Browse()
+        {
+            SrcDirectory=WindowExtensions.GetSelectedFolder(window: Window.GetWindow(View));
+        }
+        private void ModifyLandsatMtlViewModel_PropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            switch (e.PropertyName)
+            {
+                case nameof(SrcDirectory):
+                    ExcuteCmd.RaiseCanExecuteChanged();
+                    break;
+            }
+        }
+
+        private bool CanExcute()
+        {
+            return Directory.Exists(SrcDirectory);
+        }
+
+        private void Excute()
+        {
+            var directories = new List<string>() { SrcDirectory};
+            directories.AddRange(Directory.GetDirectories(SrcDirectory));
+            foreach (var item in directories)
+            {
+                var name = Path.GetFileNameWithoutExtension(item);
+                var mtlCopyPath = Path.Combine(item, $"{name}_MTL - 副本.txt");
+                if (File.Exists(mtlCopyPath))
+                {
+                    continue;
+                }
+                var mtlPath = Path.Combine(item, $"{name}_MTL.txt");
+                if (!File.Exists(mtlPath))
+                {
+                    continue;
+                }
+                File.Copy(mtlPath, mtlCopyPath,true);
+                using StreamReader sr = new StreamReader(mtlPath);
+                StringBuilder sb = new StringBuilder();
+                var firstLineStr = sr.ReadLine();
+                if (firstLineStr!=null)
+                {
+                    sb.AppendLine(firstLineStr.Replace("LANDSAT_METADATA_FILE", "L1_METADATA_FILE"));
+                }
+                var str = sr.ReadLine();
+                bool containsLevel1 = false;
+                while (str!=null)
+                {
+                    if (!string.IsNullOrEmpty(str))
+                    {
+                        if (containsLevel1)
+                        {
+                            containsLevel1=!str.Contains("END_GROUP = LEVEL1");
+                        }
+                        else
+                        {
+                            containsLevel1=str.Contains("GROUP = LEVEL1");
+                            if (!containsLevel1)
+                            {
+                                sb.AppendLine(str);
+                            }
+                            else
+                            { 
+                            }
+                        }
+                    }
+                    str= sr.ReadLine();
+                }
+                sr.Close();
+                File.WriteAllText(mtlPath, sb.ToString());
+            }
+            MessageBox.Show("OK");
+        }
+    }
+}
